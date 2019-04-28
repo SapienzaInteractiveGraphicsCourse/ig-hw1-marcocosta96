bb436e974e0c734ff09efeb5262a9402f3018b22
Marco <costa.1691388@studenti.uniroma1.it>
2019-04-28 17:52:42 +0200

    Fixes

--- homework1.js
+++ homework1.js
@@ -9,8 +9,6 @@ var numChecks = 8;
 
 var program;
 
-var c;
-
 var flag = true;
 
 var pointsArray = [];
@@ -27,7 +25,6 @@ var vertices = [
     vec4( 0.5, -0.5, -0.5, 1.0 )
 ];
 
-/*
 var vertexColors = [
     vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
     vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
@@ -38,84 +35,76 @@ var vertexColors = [
     vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
 ];
-*/
 
-var near = 0.3;
-var far = 3.0;
-var radius = 2.0;
-var theta  = 45.0;
-var phi    = 45.0;
-var dr = 5.0 * Math.PI/180.0;
+var near = 0.1;
+var far = 5.0;
+var radius = 1.0;
+var theta  = 0.0;
+var phi    = 0.0;
 
-var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
-var  aspect = 1.0;       // Viewport aspect ratio
-var scaling = 1.0;    // Scaling transformation value
-var translX = 0.0;
-var translY = 0.0;
-var translZ = 0.0;
+var fovy = 90.0;
 
-var changeShading = true;
+//Scaling
+var scaling = 0.5;
+//Translations
+var translX = 0.0, translY = 0.0, translZ = 0.0;
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
-var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
-var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc, scalingMatrixLoc, translationMatrixLoc;
 
-var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
-var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
-var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
-var materialShininess = 100.0;
-
-var ambientColor, diffuseColor, specularColor;
-
-var modelViewMatrix, projectionMatrixLeft, projectionMatrixRight, scalingMatrix, translMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc, scalingMatrixLoc, translMatrixLoc;
-//var shadingModelLoc;
-
+//Camera position
 var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-var texSize = 64;
+//light
+var lightPosition = vec4(2.0, 2.0, 2.0, 0.0);
+var lightAmbient = vec4(0.1, 0.1, 0.1, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
+//material
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 0.8, 0.4, 0.2, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 20.0;
+
+//shading
+var shadingModel = true;
+
+//texture
+var texSize = 64;
+var texCoordsArray = [];
 // Create a checkerboard pattern using floats
 var image1 = new Array()
-for (var i =0; i<texSize; i++)
-    image1[i] = new Array();
+for (var i =0; i<texSize; i++)  image1[i] = new Array();
 for (var i =0; i<texSize; i++)
     for ( var j = 0; j < texSize; j++)
-        image1[i][j] = new Float32Array(4);
-for (var i =0; i<texSize; i++)
-    for (var j=0; j<texSize; j++) {
-        var c = (((i & 0x8) == 0) ^ ((j & 0x8)  == 0));
-        image1[i][j] = [c, c, c, 1];
-    }
-
+       image1[i][j] = new Float32Array(4);
+for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
+    var c = (((i & 0x8) == 0) ^ ((j & 0x8)  == 0));
+    image1[i][j] = [c, c, c, 1];
+}
 // Convert floats to ubytes for texture
 var image2 = new Uint8Array(4*texSize*texSize);
 for ( var i = 0; i < texSize; i++ )
     for ( var j = 0; j < texSize; j++ )
        for(var k =0; k<4; k++)
             image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
-
-var texCoordsArray = [];
 var texCoord = [
-    vec2(0,0),
-    vec2(0,1),
-    vec2(1,1),
-    vec2(1,0)
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
 ];
-
 function configureTexture(image) {
     var texture = gl.createTexture();
     gl.activeTexture( gl.TEXTURE0 );
     gl.bindTexture( gl.TEXTURE_2D, texture );
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,
-        gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap( gl.TEXTURE_2D );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-        gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
 }
 
@@ -126,28 +115,28 @@ function quad(a, b, c, d) {
     var normal = vec3(normal);
 
     pointsArray.push(vertices[a]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[0]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[b]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[1]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[c]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[2]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[a]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[0]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[c]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[2]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[d]);
-    normalsArray.push(normal);
     texCoordsArray.push(texCoord[3]);
+    normalsArray.push(normal);
 }
 
 function colorCube() {
@@ -168,152 +157,155 @@ window.onload = function init() {
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    aspect = canvas.width/canvas.height;
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
     gl.enable(gl.DEPTH_TEST);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     colorCube();
 
-    var nBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
-
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal );
-
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
-    var tBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
-    var vTexCoord = gl.getAttribLocation( program, "vTexCoord");
-    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vTexCoord);
-
-    configureTexture(image2);
-
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-    scalingMatrixLoc = gl.getUniformLocation(program, "scalingMatrix");
-    translMatrixLoc = gl.getUniformLocation(program, "translMatrix");
+    var vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     var ambientProduct = mult(lightAmbient, materialAmbient);
     var diffuseProduct = mult(lightDiffuse, materialDiffuse);
     var specularProduct = mult(lightSpecular, materialSpecular);
 
-    // sliders for viewing parameters
+    gl.uniform4fv(gl.getUniformLocation(program, "vAmbientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "vDiffuseProduct"), flatten(diffuseProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "vSpecularProduct"), flatten(specularProduct));
+    gl.uniform1f(gl.getUniformLocation(program, "vShininess"), materialShininess);
 
-    document.getElementById("zFarSlider").oninput = function(event) {
-        far = event.target.value;
+    gl.uniform4fv(gl.getUniformLocation(program, "fAmbientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "fDiffuseProduct"), flatten(diffuseProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "fSpecularProduct"), flatten(specularProduct));
+    gl.uniform1f(gl.getUniformLocation(program, "fShininess"), materialShininess);
+
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
+
+    //Controllers
+    document.getElementById("NearSlider").oninput = function(event) {
+        near =this.valueAsNumber;
+        document.getElementById('labelNear').innerText = this.valueAsNumber;
     };
-    document.getElementById("zNearSlider").oninput = function(event) {
-        near = event.target.value;
-    };
-    document.getElementById("radiusSlider").oninput = function(event) {
-       radius = event.target.value;
+    document.getElementById("FarSlider").oninput = function(event) {
+        ar = this.valueAsNumber;
+        document.getElementById('labelFar').innerText = this.valueAsNumber;
     };
     document.getElementById("thetaSlider").oninput = function(event) {
-        theta = event.target.value* Math.PI/180.0;
+        theta = event.target.value * Math.PI/180.0;
+        document.getElementById('labelTheta').innerText = this.valueAsNumber;
     };
     document.getElementById("phiSlider").oninput = function(event) {
-        phi = event.target.value* Math.PI/180.0;
+        phi = event.target.value * Math.PI/180.0;
+        document.getElementById('labelPhi').innerText = this.valueAsNumber;
     };
-    document.getElementById("fovSlider").oninput = function(event) {
-        fovy = event.target.value;
+    document.getElementById("ScaleSlider").oninput = function() {
+        scaling = this.valueAsNumber;
+        document.getElementById('labelScaling').innerText = this.valueAsNumber;
     };
-    document.getElementById("scaleSlider").oninput = function(event) {
-        scaling = event.target.value;
+    document.getElementById("TranslateXSlider").oninput = function() {
+        translX = this.valueAsNumber;
+        document.getElementById('labelX').innerText = this.valueAsNumber;
     };
-    document.getElementById("traslationSliderX").oninput = function(event) {
-        translX = event.target.value;
+    document.getElementById("TranslateYSlider").oninput = function() {
+        translY = this.valueAsNumber;
+        document.getElementById('labelY').innerText = this.valueAsNumber;
     };
-    document.getElementById("traslationSliderY").oninput = function(event) {
-        translY = event.target.value;
+    document.getElementById("TranslateZSlider").oninput = function() {
+        translZ = this.valueAsNumber;
+        document.getElementById('labelZ').innerText = this.valueAsNumber;
     };
-    document.getElementById("traslationSliderZ").oninput = function(event) {
-        translZ = event.target.value;
-    };
-    document.getElementById("ShadingButton").onclick = function(){
-        changeShading = !changeShading;
-        if(changeShading) {
-            document.getElementById("currentShadingModel").innerText = "Gouraud";
+    document.getElementById("ButtonShadingModel").onclick = function() {
+        shadingModel = !shadingModel;
+        if (shadingModel) {
+            document.getElementById('currentModel').innerText = "Current Shading Model: Phong";
         }
         else {
-            document.getElementById("currentShadingModel").innerText = "Phong";
+            document.getElementById('currentModel').innerText = "Current Shading Model: Gouraud";
         }
     };
 
-    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),flatten(ambientProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"),flatten(diffuseProduct) );
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );
-    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    //Texture
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
 
-    gl.uniform1f(gl.getUniformLocation(program, "shininess"),materialShininess);
+    var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    configureTexture(image2);
 
     render();
-}
+};
 
 var render = function() {
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    const width = gl.canvas.width;
-    const height = gl.canvas.height;
-    const displayWidth = gl.canvas.clientWidth;
-    const displayHeight = gl.canvas.clientHeight;
+    var translationMatrix = [
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        translX, translY, translZ, 1.0
+    ];
 
-    // render left part
-    const dispWidthLeft = displayWidth / 2;
-    const dispHeightLeft = displayHeight;
-    const aspectLeft = dispWidthLeft/dispHeightLeft;
-    projectionMatrixLeft = perspective(fovy, aspectLeft, near, far);
-    gl.clearColor(0.1, 0.1, 0.1, 1);
-    renderSinglePart(0, 0, width/2, height, projectionMatrixLeft);
+    var scalingMatrix = [
+        scaling, 0.0, 0.0, 0.0,
+        0.0, scaling, 0.0, 0.0,
+        0.0, 0.0, scaling, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ];
 
-    //render right part
-    const dispWidthRight = displayWidth / 2;
-    const dispHeightRight = displayHeight;
-    const aspectRight = dispWidthRight/dispHeightRight;
-    const top = 1, bottom = -1;
-    const right = top * aspectRight, left = bottom * aspectRight;
-    projectionMatrixRight = ortho(left, right, bottom, top, near, far);
-    gl.clearColor(0.2, 0.2, 0.2, 1);
-    renderSinglePart(width/2, 0, width/2, height, projectionMatrixRight);
-
-    gl.uniform1f(gl.getUniformLocation(program, "changeShading"),changeShading);
-
-    requestAnimFrame(render);
-}
-
-function renderSinglePart(startX, startY, lenghtWidth, lenghtHeight, projMatrix) {
-    gl.enable(gl.SCISSOR_TEST);
-    gl.viewport(startX, startY, lenghtWidth, lenghtHeight);
-    gl.scissor(startX, startY, lenghtWidth, lenghtHeight);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    eye = vec3(radius*Math.sin(theta)*Math.cos(phi), radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+    //camera position
+    eye = vec3(radius * Math.sin(phi), radius * Math.sin(theta), radius * Math.cos(phi));
     modelViewMatrix = lookAt(eye, at , up);
-    scalingMatrix = scalem(scaling, scaling, scaling);
-    translMatrix = translate(translX, translY, translZ);
+    projectionMatrix = ortho(-1.0, 1.0, -1.0, 1.0, near, far);
 
-    gl.uniformMatrix4fv( scalingMatrixLoc, false, flatten(scalingMatrix) );
-    gl.uniformMatrix4fv( translMatrixLoc, false, flatten(translMatrix) );
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projMatrix) );
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    scalingMatrixLoc = gl.getUniformLocation(program, "scalingMatrix");
+    translationMatrixLoc = gl.getUniformLocation(program, "translationMatrix");
 
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+    gl.uniformMatrix4fv(scalingMatrixLoc, false, flatten(scalingMatrix));
+    gl.uniformMatrix4fv(translationMatrixLoc, false, translationMatrix);
+    gl.uniform1i(gl.getUniformLocation(program, "shadingModel"), shadingModel);
+
+    //Divide canvas in two parts
+    gl.enable(gl.SCISSOR_TEST);
+    var width = gl.canvas.width;
+    var height = gl.canvas.height;
+    gl.scissor(0,  height/2, width / 2, height/2);
+    gl.viewport(0,  height/2, width / 2, height/2);
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+
+    projectionMatrix = perspective(fovy, canvas.width/canvas.height, near, far);
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    gl.scissor(width/2, height/2, width/2, height/2);
+    gl.viewport(width/2, height/2, width/2, height/2);
+
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+
+    requestAnimationFrame(render);
 }
